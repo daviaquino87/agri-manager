07a95d5497d105260c75c34588d030e660f6f577
daviaquino87 <davi26031@gmail.com>
2025-06-11 17:53:46 -0300

    chore: create update method on repository

--- src/infra/database/prisma/repositories/prisma-producer.repository.ts
+++ src/infra/database/prisma/repositories/prisma-producer.repository.ts
@@ -2,17 +2,41 @@ import { IProducer } from '@/modules/producers/entities/producer.entity';
 import { ProducerRepository } from '@/modules/producers/repositories/producer.repository';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class PrismaProducerRepository implements ProducerRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async create(createProducerDto: IProducer): Promise<void> {
-    await this.prismaService.producer.create({
+  async create(
+    createProducerDto: IProducer,
+    txn?: Prisma.TransactionClient,
+  ): Promise<IProducer> {
+    const connection = txn ?? this.prismaService;
+
+    return connection.producer.create({
       data: {
         id: String(createProducerDto.id),
         name: createProducerDto.name,
         document: createProducerDto.document,
+      },
+    });
+  }
+
+  async update(
+    id: string,
+    updateProducerDto: Partial<IProducer>,
+    txn?: Prisma.TransactionClient,
+  ): Promise<IProducer> {
+    const connection = txn ?? this.prismaService;
+
+    return connection.producer.update({
+      where: {
+        id,
+      },
+      data: {
+        name: updateProducerDto.name,
+        document: updateProducerDto.document,
       },
     });
   }
--- src/modules/producers/repositories/producer.repository.ts
+++ src/modules/producers/repositories/producer.repository.ts
@@ -1,5 +1,10 @@
 import { IProducer } from '../entities/producer.entity';
 
 export abstract class ProducerRepository {
-  abstract create(createProducerDto: IProducer): Promise<void>;
+  abstract create(createProducerDto: IProducer, txn?: any): Promise<IProducer>;
+  abstract update(
+    id: string,
+    updateProducerDto: Partial<IProducer>,
+    txn?: any,
+  ): Promise<IProducer>;
 }
