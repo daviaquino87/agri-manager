ee5a6483c38ccdf1b2b27faa052527e234c102d3
daviaquino87 <davi26031@gmail.com>
2025-06-12 11:01:38 -0300

    chore:added document mask as mandatory

--- src/common/decorators/validade-cpf-or-cnpj.decorator.ts
+++ src/common/decorators/validade-cpf-or-cnpj.decorator.ts
@@ -4,16 +4,23 @@ import {
   ValidatorConstraint,
   ValidatorConstraintInterface,
 } from 'class-validator';
-import { isCPFOrCNPJ } from 'brazilian-values';
+import { isCPF, isCNPJ } from 'brazilian-values';
 
-@ValidatorConstraint({ name: 'IsNotBlank' })
-export class IsCPFOrCNPJContrain implements ValidatorConstraintInterface {
+@ValidatorConstraint({ name: 'IsCPFOrCNPJ' })
+export class IsCPFOrCNPJConstrain implements ValidatorConstraintInterface {
   validate(value: any) {
-    return isCPFOrCNPJ(value);
+    if (typeof value !== 'string') return false;
+
+    const isFormattedCPF =
+      /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(value) && isCPF(value);
+    const isFormattedCNPJ =
+      /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/.test(value) && isCNPJ(value);
+
+    return isFormattedCPF || isFormattedCNPJ;
   }
 
   defaultMessage(): string {
-    return 'Ops! o documento informado está inválido.';
+    return 'Ops! O CPF ou CNPJ informado é inválido ou está sem máscara.';
   }
 }
 
@@ -24,7 +31,7 @@ export function IsCPFOrCNPJ(validationOptions?: ValidationOptions) {
       propertyName,
       constraints: [],
       options: validationOptions,
-      validator: IsCPFOrCNPJContrain,
+      validator: IsCPFOrCNPJConstrain,
     });
   };
 }
